623b68946dec589863ea715c761734de300be786
s-guridi <santiago.guridi@gmail.com>
2017-12-07 20:36:44 -0300

    Added comments for better understanding

--- Hacedor ppt/init.py
+++ Hacedor ppt/init.py
@@ -8,28 +8,31 @@ END_PPT = 'ppt_listo.pptx'
 SLIDE_SIZE = 730
 
 ADDRS = {}
-LECTURES = {}
+READINGS = {}
 
 PPT_TITLE = sys.argv[1] #Change when running on server
 
 #When extracts from the web
 if len(sys.argv) > 2:
+	#Data obtained from www.eucaristiadiaria.cl using scraper.py
 	DATE = scrapper.FECHA
 
-	
+	#Bible readings addresses
 	ADDRS['primera_lectura'] = scrapper.DIR_PRIMERA_LECTURA
 	ADDRS['salmo'] = scrapper.DIR_SALMO
 	ADDRS['segunda_lectura'] = scrapper.DIR_SEGUNDA_LECTURA
 	ADDRS['evangelio'] = scrapper.DIR_EVANGELIO
 
-	
-	LECTURES['primera_lectura'] = scrapper.PRIMERA_LECTURA
-	LECTURES['salmo'] = scrapper.SALMO
-	LECTURES['segunda_lectura'] = scrapper.SEGUNDA_LECTURA
-	LECTURES['evangelio'] = scrapper.EVANGELIO
+	#Readings obtained from www.eucaristiadiaria.cl
+	READINGS['primera_lectura'] = scrapper.PRIMERA_LECTURA
+	READINGS['salmo'] = scrapper.SALMO
+	READINGS['segunda_lectura'] = scrapper.SEGUNDA_LECTURA
+	READINGS['evangelio'] = scrapper.EVANGELIO
 
 else:
 	with open("lecturas.txt", 'r', encoding = 'utf-8') as file:
+		#Data obtained from a hardcoded text file
+
 		DATE = iterar(file, "PRIMERA_LECTURA_DIR = ")
 
 		ADDRS['primera_lectura'] = iterar(file, "SALMO_DIR = ")
@@ -37,12 +40,11 @@ else:
 		ADDRS['segunda_lectura'] = iterar(file, 'EVANGELIO_DIR = ')
 		ADDRS['evangelio'] = iterar(file, 'PRIMERA_LECTURA')     
 
-
-		LECTURES['primera_lectura'] = iterar(file, "SALMO")
-		LECTURES['salmo'] = iterar(file, 'SEGUNDA_LECTURA')
-		LECTURES['segunda_lectura'] = iterar(file, 'EVANGELIO')
-		LECTURES['evangelio'] = iterar(file, 'SANTIAGO SCRAPPERS')
+		READINGS['primera_lectura'] = iterar(file, "SALMO")
+		READINGS['salmo'] = iterar(file, 'SEGUNDA_LECTURA')
+		READINGS['segunda_lectura'] = iterar(file, 'EVANGELIO')
+		READINGS['evangelio'] = iterar(file, 'SANTIAGO SCRAPPERS')
 
 
 if __name__ == '__main__':
-	Maker(LECTURES, BASE_PPT, END_PPT, SLIDE_SIZE, ADDRS, DATE, PPT_TITLE)
+	Maker(READINGS, BASE_PPT, END_PPT, SLIDE_SIZE, ADDRS, DATE, PPT_TITLE)
