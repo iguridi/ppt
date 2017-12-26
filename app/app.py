1713c2bcf8f85439bc369b2b864d0835757072e3
s-guridi <santiago.guridi@gmail.com>
2017-12-26 17:24:23 -0300

    Added the link maker, a 'month replacer' for spanish and more...

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import datetime
 import requests
 from flask import Flask, render_template, request, send_from_directory, current_app
 from bs4 import BeautifulSoup
@@ -17,34 +18,35 @@ def next_sunday():
     BASE_PPT = directory + folder + '/plantilla python.pptx'
     OUTPUT_PPT = directory + folder + '/ppt_listo.pptx'
     SLIDE_SIZE = 730
-    PPT_TITLE = 'hooola'
-    ADDRS = scrapper.ADDRS
-    READINGS = scrapper.READINGS
-<<<<<<< HEAD
-<<<<<<< HEAD
     if request.method == "POST":
-        print(request.form)
 
-    # DATE = scrapper.FECHA
-=======
-    DATE = scrapper.FECHA
+        PPT_TITLE = request.form['title']
+        date = request.form['date']
+        #Convert date to datetime object
+        date = datetime.datetime.strptime(date, '%Y-%m-%d')
+        url = make_url(date)
+        date = str(date.day) + ' ' + monthName(date.month) + ' ' + str(date.year)
+        ADDRS, READINGS= scrapper.run(url)
 
-    if request.method == "POST":
-        print(request.form)
 
->>>>>>> Changed requests
-=======
-    DATE = scrapper.FECHA
-
-    if request.method == "POST":
-        print(request.form)
-
->>>>>>> a1335a1b2e96ce19034f1c95dc576cd8c469a2da
-    pptMaker.Maker(READINGS, BASE_PPT, OUTPUT_PPT, SLIDE_SIZE, ADDRS, DATE, PPT_TITLE)
-
+    pptMaker.Maker(READINGS, BASE_PPT, OUTPUT_PPT, SLIDE_SIZE, ADDRS, date, PPT_TITLE)
     path = current_app.root_path + folder
     return send_from_directory(directory=path, filename='ppt_listo.pptx',
-        as_attachment=True, attachment_filename=DATE + '.pptx')
+        as_attachment=True, attachment_filename=date + '.pptx')
+
+
+def make_url(date):
+    BASE_URL = 'http://www.eucaristiadiaria.cl/'
+    if date.weekday() == 0:
+        url = BASE_URL + 'domingo.php'
+    else:
+        url = BASE_URL + 'dia_cal.php?fecha=' + str(date)
+    return url
+
+
+def monthName(month_number):
+    months = ["", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    return months[month_number]
 
 
 
@@ -52,21 +54,6 @@ def next_sunday():
 def index():
     errors = []
     results = {}
-    print('blabla')
-    if request.method == "POST":
-        # get url that the user has entered
-        try:
-            url = request.form['url']
-            print(url, 'blabla')
-            r = requests.get(url)
-            print(r.text)
-        except:
-            errors.append(
-                "Unable to get URL. Please make sure it's valid and try again."
-            )
-            return render_template('index.html', errors=errors, results=results)
-        raw = BeautifulSoup(r.text, 'html.parser').get_text()
-        return render_template('index.html', errors=[raw], results=raw)
     return render_template('index.html', errors=errors, results=results)
 
 
