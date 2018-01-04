7c1ce3499f1cc7442b46db761938f0d87180b211
s-guridi <santiago.guridi@gmail.com>
2018-01-04 17:01:35 -0300

    Created the posibility to move to the next month

--- app/static/index.js
+++ app/static/index.js
@@ -2,46 +2,74 @@ var date = new Date();
 var year = date.getFullYear();
 var month = date.getMonth();
 
-
-function daysInMonth() {
-    month = month + 1;
-    return new Date(year, month, 0).getDate() + 1;
+//To back-end
+function makeDate(day, m){
+    realMonth = m + 1
+    return year + '-' + realMonth + '-' + day;
 }
 
-function makeDate(day){
-    return year + '-' + month + '-' + day;
-}
-
+//Front-end
 var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-function firstDayMonth(date, month, year) {
+function daysInMonth(m, y) {
+    if (m == 1){
+        return new Date(y, m, 28).getDate();
+    }
+    return new Date(y, m, 0).getDate();
+}
+
+
+function firstDayMonth(m, y) {
     var dateObj = new Date();
-    dateObj.setFullYear(year);
-    dateObj.setMonth(month);
+    dateObj.setFullYear(y);
+    dateObj.setMonth(m);
     dateObj.setDate(1);
     var first_day = dateObj.getDay();
     return first_day;
 }
 
+var pastMonth = new Object();
+var displayMonth = new Object();
+
+function makeCalendar(_, selectedMonth = "current") {
+    if (selectedMonth == "current") {
+        pastMonth.startWeekDay = firstDayMonth(month-1, year);
+        pastMonth.monthLength = daysInMonth(month, year);
+        pastMonth.monthNumber = month - 1;
+
+        displayMonth.startWeekDay = firstDayMonth(month, year);
+        displayMonth.monthLength = daysInMonth(month, year);
+        displayMonth.monthNumber = month;
+    }
+    else if (selectedMonth == "next") {
+        pastMonth.startWeekDay = firstDayMonth(month, year);
+        pastMonth.monthLength = daysInMonth(month, year);
+        pastMonth.monthNumber = month;
+
+        displayMonth.startWeekDay = firstDayMonth(month+1, year),
+        displayMonth.monthLength = daysInMonth(month+1, year)
+        displayMonth.monthNumber = month + 1;
 
 
-var lastDaySelected;
-window.onload = function makeCalendar() {
+    }
     //Set set year
-    var html_year = document.getElementById("month").innerHTML = year;
+    var html_year = document.getElementById("year").innerHTML = year;
     //Setting month
-    var html_month = document.getElementById("month").innerHTML = months[month];
-    //Setting dates
-    firstDay = firstDayMonth("1", month, year);
+    var html_month = document.getElementById("month").innerHTML = months[displayMonth.monthNumber];
+    //Setting dates 
     var daysContainer = document.getElementById("day_nums");
-    for (i = 28; i < firstDay + 27; i++) {
+    daysContainer.innerHTML = '';
+
+    var n = pastMonth.monthLength - displayMonth.startWeekDay + 2;// Days of the past month to show
+    var a = displayMonth.startWeekDay + n - 1;
+    console.log(n)
+    for (i = n; i < a; i++) {
         var dayPastMonth = document.createElement("div");
         dayPastMonth.classList.add("day");
         dayPastMonth.textContent = i;
         daysContainer.appendChild(dayPastMonth);
     }
-    month_length = daysInMonth(month, year);
-    for (i = 1; i <= month_length; i++) {
+    for (i = 1; i <= displayMonth.monthLength; i++) {
         var currentMonthDay = document.createElement("button");
         currentMonthDay.setAttribute("id", i);
         currentMonthDay.classList.add("day", "available");
@@ -51,7 +79,29 @@ window.onload = function makeCalendar() {
     }
 };
 
+window.onload = makeCalendar;
+
+function nextMonth() {
+    makeCalendar('a', "next");
+    //SHOW return button and HIDE    next button
+    var nextButton = document.getElementById("next-month");
+    nextButton.style.display = 'none';
+    var previousButton = document.getElementById("previous-month");
+    previousButton.style.display = 'inline-block';
+
+}
+
+function previousMonth() {
+    makeCalendar('a', "current");
+    //HIDE return button and SHOW next button
+    var nextButton = document.getElementById("next-month");
+    nextButton.style.display = 'inline-block';
+    var previousButton = document.getElementById("previous-month");
+    previousButton.style.display = 'none';
+}
+
 function daySelected(event) {
+    var lastDaySelected;
     var days_available = document.getElementsByClassName("available");
     //Remove the past day selected
     for (var n = 0; n < days_available.length; n++) {
@@ -65,21 +115,18 @@ function daySelected(event) {
     lastDaySelected = event.target.id;
     //Add the value to the form-input
     var dateSelected = document.getElementById("date");
-    dateSelected.value = makeDate(event.target.id, month, year);
-    console.log(dateSelected.value)
+    dateSelected.value = makeDate(event.target.id, displayMonth.monthNumber, year);
     //Then show next step
     let step2 = document.getElementsByClassName("step2")
     for (i = 0; i < step2.length; i++) {
         step2[i].style.visibility = "visible";
         step2[i].style.opacity = "1";
     }
-
 }
 
 function titleSelected() {
     let titleInput = document.getElementById("title-input");
     let step3 = document.getElementsByClassName("step3")
-    console.log(step3)
     for (i = 0; i < step3.length; i++) {
         step3[i].style.visibility = "visible";
         step3[i].style.opacity = "1";
