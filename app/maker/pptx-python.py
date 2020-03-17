d8989d6bca970374a6282bc3e2b3329037a2d453
iguridi <iguridi@uc.cl>
2020-03-17 10:59:20 -0300

    style: yapf pptx-python

--- app/maker/pptx-python.py
+++ app/maker/pptx-python.py
@@ -2,32 +2,35 @@ from pptx import Presentation
 
 
 def analyze_ppt(input, output):
-    """ Take the input file and analyze the structure.
+    """
+    Take the input file and analyze the structure.
     The output file contains marked up information to make it easier
     for generating future powerpoint templates.
     """
     prs = Presentation(input)
-    # Each powerpoint file has multiple layouts
-    # Loop through them all and  see where the various elements are
+    # each powerpoint file has multiple layouts
+    # loop through them all and  see where the various elements are
     for index, _ in enumerate(prs.slide_layouts):
         slide = prs.slides.add_slide(prs.slide_layouts[index])
-        # Not every slide has to have a title
+        # not every slide has to have a title
         try:
             title = slide.shapes.title
             title.text = 'Title for Layout {}'.format(index)
         except AttributeError:
             print("No Title for Layout {}".format(index))
-        # Go through all the placeholders and identify them by index and type
+        # go through all the placeholders and identify them by index and type
         for shape in slide.placeholders:
             if shape.is_placeholder:
                 phf = shape.placeholder_format
-                # Do not overwrite the title which is just a special placeholder
+                # do not overwrite the title which is just a special placeholder
                 try:
                     if 'Title' not in shape.text:
-                        shape.text = 'Placeholder index:{} type:{}'.format(phf.idx, shape.name)
+                        shape.text = 'Placeholder index:{} type:{}'.format(
+                            phf.idx, shape.name)
                 except AttributeError:
                     print("{} has no text attribute".format(phf.type))
                 print('{} {}'.format(phf.idx, shape.name))
     prs.save(output)
+
 
 analyze_ppt('plantilla python.pptx', 'probando.pptx')
